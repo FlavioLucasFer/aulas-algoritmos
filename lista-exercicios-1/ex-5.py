4ae2fcf018f5a4887e82bce2f53ffff079b1980f
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2021-11-06 10:19:23 -0300

    Exercício 5 da lista de exercícios 1

---
+++ lista-exercicios-1/ex-5.py
@@ -0,0 +1,24 @@
+'''
+Escreva um programa que calcule todas as operações aritméticas básicas dos valores de duas variáveis 
+(pode-se admitir valores quaisquer) e armazene cada resultado em uma outra variável diferente. 
+Ao final do programa, exiba os valores resultantes em funções “print”.
+'''
+
+num1 = 70
+num2 = 77
+
+soma = num1 + num2
+subtracao = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+divInteira = num1 // num2
+exp = num1 ** num2
+mod = num1 % num2
+
+print('Soma:', soma)
+print('Subtração:', subtracao)
+print('Multiplicação:', mult)
+print('Divisão:', div)
+print('Exponenciação:', exp)
+print('Divisão inteira: ',divInteira)
+print('Resto de divisão (mod):', mod)
